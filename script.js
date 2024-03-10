e594e45435d679d41c44f072174c18e28da91e51
reniwerner <77681683+reniwerner@users.noreply.github.com>
2024-03-10 13:19:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    function fillData() {
+     
+      $('#name').text("Reni Werner");
+      $('#major').text("Computer Graphics Technology");
+      $('#year').text("Senior");
+  
+     
+      $('#location').html("<em>Indiana</em>");
+    }
+  
+    fillData(); 
+  });
